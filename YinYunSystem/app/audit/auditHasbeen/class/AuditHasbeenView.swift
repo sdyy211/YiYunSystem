94a46c16ddc0e1bfb3cf7bb2bb7b7ef61d52a7f2
MacSourceTree <1078805356@qq.com>
2016-02-24 16:34:35 +0800

    zhaoql 自适应页面

--- YinYunSystem/app/audit/auditHasbeen/class/AuditHasbeenView.swift
+++ YinYunSystem/app/audit/auditHasbeen/class/AuditHasbeenView.swift
@@ -71,7 +71,7 @@ class AuditHasbeenView: UIView,UITableViewDelegate,UITableViewDataSource{
     
     override func drawRect(rect: CGRect) {
         // Drawing code
-        let leftWith:CGFloat = 30
+        let leftWith:CGFloat = 15
         let with = (CGRectGetWidth(self.frame) - leftWith*2)
         let h = 6*50+80
         let xh = CGRectGetHeight(self.frame)
